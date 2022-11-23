fd0f61e0b26a5329e94f1ab39ad6c20c38a12ac4
ArsenalAlex108 <107184385+ArsenalAlex108@users.noreply.github.com>
2022-11-23 13:57:46 +0700

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -294,9 +294,13 @@ void MainThread::search() {
   }
 
   sync_cout << "bestmove " << UCI::move(rootPos, bestThread->rootMoves[0].pv[0]);
+  UCI::StrOut("bestmove " + UCI::move(rootPos, bestThread->rootMoves[0].pv[0]));
 
   if (bestThread->rootMoves[0].pv.size() > 1 || bestThread->rootMoves[0].extract_ponder_from_tt(rootPos))
+  {
       std::cout << " ponder " << UCI::move(rootPos, bestThread->rootMoves[0].pv[1]);
+      UCI::StrOut(" ponder " + UCI::move(rootPos, bestThread->rootMoves[0].pv[1]));
+  }
 
   std::cout << sync_endl;
 }
