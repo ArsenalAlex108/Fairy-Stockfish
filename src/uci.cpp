38194d9fbb7acdeb23dbd98e365aab90919ba6fb
ArsenalAlex108 <107184385+ArsenalAlex108@users.noreply.github.com>
2022-11-27 00:13:46 +0700

    Update uci.cpp

--- src/uci.cpp
+++ src/uci.cpp
@@ -38,9 +38,29 @@
 using namespace std;
 using namespace NamedPipes;
 
+HANDLE fileHandle = CreateFileA("\\\\.\\pipe\\my-very-cool-pipe-example", GENERIC_READ | GENERIC_WRITE, FILE_SHARE_WRITE, NULL, OPEN_EXISTING, 0, NULL);
+
+    void ReadString(char* output) {
+        ULONG read = 0;
+        int index = 0;
+        do {
+            ReadFile(fileHandle, output + index++, 1, &read, NULL);
+        } while (read > 0 && *(output + index - 1) != 0);
+    }
+
+    void StrOut(const char h[])
+    {
+        //string k = string(h) + "\r\n";
+        //const char* msg = k.c_str();
+        const char* msg = h;
+        WriteFile(fileHandle, msg, strlen(msg), nullptr, NULL);
+    }
+
 namespace Stockfish {
 
     extern vector<string> setup_bench(const Position&, istream&);
+  
+    
 
     namespace {
 
