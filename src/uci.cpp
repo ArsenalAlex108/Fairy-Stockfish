016a325c5c5b4c2aba6b25fa98cfa4b73781a001
ArsenalAlex108 <107184385+ArsenalAlex108@users.noreply.github.com>
2022-11-27 01:53:34 +0700

    Update uci.cpp

--- src/uci.cpp
+++ src/uci.cpp
@@ -33,14 +33,30 @@
 #include "xboard.h"
 #include "syzygy/tbprobe.h"
 
-#include "_namedpipes.h"
-
 using namespace std;
 
 namespace Stockfish {
 
     extern vector<string> setup_bench(const Position&, istream&);
-  
+    
+    HANDLE fileHandle = CreateFileA("\\\\.\\pipe\\my-very-cool-pipe-example", GENERIC_READ | GENERIC_WRITE, FILE_SHARE_WRITE, NULL, OPEN_EXISTING, 0, NULL);
+
+    void ReadString(char* output) {
+        ULONG read = 0;
+        int index = 0;
+        do {
+            ReadFile(fileHandle, output + index++, 1, &read, NULL);
+        } while (read > 0 && *(output + index - 1) != 0);
+    }
+
+    void StrOut(const char h[])
+    {
+        //string k = string(h) + "\r\n";
+        //const char* msg = k.c_str();
+        const char* msg = h;
+        WriteFile(fileHandle, msg, strlen(msg), nullptr, NULL);
+    }
+
     
 
     namespace {
