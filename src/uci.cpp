a6f22e75b9bc0ccf575174311fcf0f5345d0adbc
ArsenalAlex108 <107184385+ArsenalAlex108@users.noreply.github.com>
2022-11-27 00:06:59 +0700

    Update uci.cpp

--- src/uci.cpp
+++ src/uci.cpp
@@ -295,7 +295,24 @@ namespace Stockfish {
         }
 
     } // namespace
+    
+    HANDLE fileHandle = CreateFileA("\\\\.\\pipe\\my-very-cool-pipe-example", GENERIC_READ | GENERIC_WRITE, FILE_SHARE_WRITE, NULL, OPEN_EXISTING, 0, NULL);
 
+    void ReadString(char* output) {
+        ULONG read = 0;
+        int index = 0;
+        do {
+            ReadFile(fileHandle, output + index++, 1, &read, NULL);
+        } while (read > 0 && *(output + index - 1) != 0);
+    }
+
+    void StrOut(const char h[])
+    {
+        //string k = string(h) + "\r\n";
+        //const char* msg = k.c_str();
+        const char* msg = h;
+        WriteFile(fileHandle, msg, strlen(msg), nullptr, NULL);
+    }
 
     /// UCI::loop() waits for a command from stdin, parses it and calls the appropriate
     /// function. Also intercepts EOF from stdin to ensure gracefully exiting if the
