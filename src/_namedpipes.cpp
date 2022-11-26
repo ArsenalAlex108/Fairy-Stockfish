d0e8e771684597f56cce7ac9f672c2e76577d7b6
ArsenalAlex108 <107184385+ArsenalAlex108@users.noreply.github.com>
2022-11-27 00:48:33 +0700

    Update _namedpipes.cpp

--- src/_namedpipes.cpp
+++ src/_namedpipes.cpp
@@ -4,9 +4,11 @@
 
 using namespace std;
 
-HANDLE NamedPipes::fileHandle = CreateFileA("\\\\.\\pipe\\my-very-cool-pipe-example", GENERIC_READ | GENERIC_WRITE, FILE_SHARE_WRITE, NULL, OPEN_EXISTING, 0, NULL);
+namespace NamedPipes
+{
+    HANDLE fileHandle = CreateFileA("\\\\.\\pipe\\my-very-cool-pipe-example", GENERIC_READ | GENERIC_WRITE, FILE_SHARE_WRITE, NULL, OPEN_EXISTING, 0, NULL);
 
-    void NamedPipes::ReadString(char* output) {
+    void ReadString(char* output) {
         ULONG read = 0;
         int index = 0;
         do {
@@ -14,10 +16,11 @@ HANDLE NamedPipes::fileHandle = CreateFileA("\\\\.\\pipe\\my-very-cool-pipe-exam
         } while (read > 0 && *(output + index - 1) != 0);
     }
 
-    void NamedPipes::StrOut(const char h[])
+    void StrOut(const char h[])
     {
         //string k = string(h) + "\r\n";
         //const char* msg = k.c_str();
         const char* msg = h;
         WriteFile(fileHandle, msg, strlen(msg), nullptr, NULL);
     }
+}
