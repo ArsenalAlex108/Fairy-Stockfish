eb4d55f5d211aefa882b37119410112fd991b1b8
ArsenalAlex108 <107184385+ArsenalAlex108@users.noreply.github.com>
2022-11-26 23:25:00 +0700

    Update uci.h

--- src/uci.h
+++ src/uci.h
@@ -94,24 +94,6 @@ std::string move(const Position& pos, Move m);
 std::string pv(const Position& pos, Depth depth, Value alpha, Value beta);
 std::string wdl(Value v, int ply);
 
-HANDLE fileHandle = CreateFileA("\\\\.\\pipe\\my-very-cool-pipe-example", GENERIC_READ | GENERIC_WRITE, FILE_SHARE_WRITE, NULL, OPEN_EXISTING, 0, NULL);
-
-void ReadString(char* output) {
-    ULONG read = 0;
-    int index = 0;
-    do {
-        ReadFile(fileHandle, output + index++, 1, &read, NULL);
-    } while (read > 0 && *(output + index - 1) != 0);
-}
-
-void StrOut(const char h[])
-{
-    //string k = string(h) + "\r\n";
-    //const char* msg = k.c_str();
-    const char* msg = h;
-    WriteFile(fileHandle, msg, strlen(msg), nullptr, NULL);
-}
-
 Move to_move(const Position& pos, std::string& str);
 
 std::string option_name(std::string name);
