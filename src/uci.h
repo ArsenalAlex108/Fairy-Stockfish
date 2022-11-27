5097e9ba1b2d80fe3c0a2ed10c1173322c2af91c
ArsenalAlex108 <107184385+ArsenalAlex108@users.noreply.github.com>
2022-11-27 11:54:48 +0700

    Update uci.h

--- src/uci.h
+++ src/uci.h
@@ -104,6 +104,7 @@ bool is_valid_option(UCI::OptionsMap& options, std::string& name);
 extern HANDLE fileHandle;
 extern void ReadString(char* output);
 extern void StrOut(const char h[]);
+extern bool bool_get;
   
 extern UCI::OptionsMap Options;
 
